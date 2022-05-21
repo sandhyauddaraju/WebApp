09555f50adbd6304a13af9a5d4840b0c8b65c19e
sandhyauddaraju <67341722+sandhyauddaraju@users.noreply.github.com>
2022-05-21 07:50:26 -0400

    Update ItemCategoriesController.cs

--- Controllers/ItemCategoriesController.cs
+++ Controllers/ItemCategoriesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+ [Authorize]
     public class ItemCategoriesController : Controller
     {
         private readonly ApplicationDBContext _context;
